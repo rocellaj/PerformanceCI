c0ef776fde34c097938ee96c9e7723bcad11b56a
Rocella Jimenez <rocella.jimenez@rbc.com>
2018-11-07 16:31:55 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+class App extends Component {
+   constructor(){
+       super();
+       this.state ={users: []};
+   }
+   componentDidMount() {
+          fetch('/users')
+            .then(res => {
+                console.log(res);
+                return res.json()
+             })
+            .then(users => { 
+                console.log(users); 
+                this.setState({ users })
+             });
+         }
+   render() {
+        return (
+            <div className="App">
+                <h1>Users</h1>
+                {this.state.users.map(user =>
+                <div key={user.id}>user: {user.name} Password: {user.password}</div>
+              )}
+            </div>
+        );
+    }
+}
+export default App;
