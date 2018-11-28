d3b8a8330f1a115a4a4e13cace593e75eae1ada2
Rocella Jimenez <rocella.jimenez@rbc.com>
2018-11-28 12:04:23 -0500

    updated host and port

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const db = require('./routes/queries')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3001
-const host = '127.0.0.1'
+// const port = 3001
+// const host = '127.0.0.1'
+
+app.set('port', process.env.PORT || 3001);
+app.set('host', process.env.HOST || '0.0.0.0');
 
 app.use(bodyParser.json())
 app.use(
@@ -23,6 +26,10 @@ app.post('/expense', db.createExpense)
 //     console.log(`App running on port ${port}.`)
 //   })
 
-app.listen(port, host, () => { 
-  console.log(`App running on ${host}:${port}.`)
-})
+// app.listen(port, host, () => { 
+//   console.log(`App running on ${host}:${port}.`)
+// })
+
+app.listen(app.get('port'), app.get('host'), function(){
+  console.log("Express server listening on port " + app.get('port') + app.get ('host'));
+});
